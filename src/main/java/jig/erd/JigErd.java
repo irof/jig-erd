77862953c6a06cd60b1088d0eaab49911177244b
irof <irof.ocean@gmail.com>
2020-08-06 22:16:07 +0900

    refactor
    connectionの取得を1回にする

--- src/main/java/jig/erd/JigErd.java
+++ src/main/java/jig/erd/JigErd.java
@@ -64,49 +64,44 @@ public class JigErd {
     }
 
     private void loadSchemaData() {
-        loadTables();
-        loadReferences();
-    }
-
-    private void loadTables() {
         try (Connection conn = dataSource.getConnection()) {
-            try (Statement t = conn.createStatement();
-                 ResultSet rs = t.executeQuery("SELECT " +
-                         " TABLE_SCHEMA, TABLE_NAME, REMARKS" +
-                         " FROM INFORMATION_SCHEMA.TABLES" +
-                         " WHERE TABLE_TYPE = 'TABLE'")) {
-                while (rs.next()) {
-                    Schema schema = repository.getSchema(rs.getString("TABLE_SCHEMA"));
-                    repository.registerEntity(schema, rs.getString("TABLE_NAME"), rs.getString("REMARKS"));
-                }
-            }
-
+            loadTables(conn);
+            loadReferences(conn);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private void loadReferences() {
-        try (Connection conn = dataSource.getConnection()) {
-            try (Statement t = conn.createStatement();
-                 ResultSet rs = t.executeQuery("SELECT"
-                         + " FKTABLE_SCHEMA, FKTABLE_NAME, FKCOLUMN_NAME,"
-                         + " PKTABLE_SCHEMA, PKTABLE_NAME, PKCOLUMN_NAME"
-                         + " FROM INFORMATION_SCHEMA.CROSS_REFERENCES")) {
-                while (rs.next()) {
-                    ColumnIdentifier from = new ColumnIdentifier(
-                            rs.getString("FKTABLE_SCHEMA"),
-                            rs.getString("FKTABLE_NAME"),
-                            rs.getString("FKCOLUMN_NAME"));
-                    ColumnIdentifier to = new ColumnIdentifier(
-                            rs.getString("PKTABLE_SCHEMA"),
-                            rs.getString("PKTABLE_NAME"),
-                            rs.getString("PKCOLUMN_NAME"));
-                    repository.registerRelation(from, to);
-                }
+    private void loadTables(Connection conn) throws SQLException {
+        try (Statement t = conn.createStatement();
+             ResultSet rs = t.executeQuery("SELECT " +
+                     " TABLE_SCHEMA, TABLE_NAME, REMARKS" +
+                     " FROM INFORMATION_SCHEMA.TABLES" +
+                     " WHERE TABLE_TYPE = 'TABLE'")) {
+            while (rs.next()) {
+                Schema schema = repository.getSchema(rs.getString("TABLE_SCHEMA"));
+                repository.registerEntity(schema, rs.getString("TABLE_NAME"), rs.getString("REMARKS"));
             }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        }
+    }
+
+    private void loadReferences(Connection conn) throws SQLException {
+        try (Statement t = conn.createStatement();
+             ResultSet rs = t.executeQuery("SELECT"
+                     + " FKTABLE_SCHEMA, FKTABLE_NAME, FKCOLUMN_NAME,"
+                     + " PKTABLE_SCHEMA, PKTABLE_NAME, PKCOLUMN_NAME"
+                     + " FROM INFORMATION_SCHEMA.CROSS_REFERENCES")) {
+            while (rs.next()) {
+                ColumnIdentifier from = new ColumnIdentifier(
+                        rs.getString("FKTABLE_SCHEMA"),
+                        rs.getString("FKTABLE_NAME"),
+                        rs.getString("FKCOLUMN_NAME"));
+                ColumnIdentifier to = new ColumnIdentifier(
+                        rs.getString("PKTABLE_SCHEMA"),
+                        rs.getString("PKTABLE_NAME"),
+                        rs.getString("PKCOLUMN_NAME"));
+                repository.registerRelation(from, to);
+            }
         }
     }
 }
