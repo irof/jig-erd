c11f5dad67c49dc2651e1e107f1f75c71858a1c7
irof <irof.ocean@gmail.com>
2022-06-14 22:54:22 +0900

    h2 v2対応

--- src/main/java/jig/erd/infrastructure/database/H2Loader.java
+++ src/main/java/jig/erd/infrastructure/database/H2Loader.java
@@ -8,6 +8,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/**
+ * H2 Database Engine v2向けのLoaderです。
+ *
+ * @link <a href="https://www.h2database.com/html/systemtables.html">h2のINFORMATION_SCHEMAの説明</a>
+ */
 public class H2Loader {
     private Repository repository;
 
@@ -22,10 +27,11 @@ public class H2Loader {
 
     void loadTables(Connection conn) throws SQLException {
         try (Statement t = conn.createStatement();
-             ResultSet rs = t.executeQuery("SELECT " +
+             ResultSet rs = t.executeQuery("SELECT" +
                      " TABLE_SCHEMA, TABLE_NAME, REMARKS" +
                      " FROM INFORMATION_SCHEMA.TABLES" +
-                     " WHERE TABLE_TYPE = 'TABLE'")) {
+                     " WHERE TABLE_TYPE = 'BASE TABLE'" +
+                     " AND TABLE_SCHEMA <> 'INFORMATION_SCHEMA'")) {
             while (rs.next()) {
                 Schema schema = repository.getSchema(rs.getString("TABLE_SCHEMA"));
                 repository.registerEntity(schema, rs.getString("TABLE_NAME"), rs.getString("REMARKS"));
@@ -35,19 +41,34 @@ public class H2Loader {
 
     void loadReferences(Connection conn) throws SQLException {
         try (Statement t = conn.createStatement();
-             ResultSet rs = t.executeQuery("SELECT"
-                     + " FKTABLE_SCHEMA, FKTABLE_NAME, FKCOLUMN_NAME,"
-                     + " PKTABLE_SCHEMA, PKTABLE_NAME, PKCOLUMN_NAME"
-                     + " FROM INFORMATION_SCHEMA.CROSS_REFERENCES")) {
+             // key_column_usage には制約で使用されているカラムの順番が記録されている。
+             //   複数キーからなるFK/PKで列の組み合わせを特定するためにJOINする必要がある。
+             // referential_constraints でFKの制約と対象になるPKの制約を特定
+             //   key_column_usageと結合することで列を特定する。
+             //   constraint_nameは異なるschemaで同じ名前が使用できるので、schema/name両方で結合する必要がある。
+             ResultSet rs = t.executeQuery("SELECT" +
+                     " k.table_schema fk_schema, k.table_name fk_table, k.column_name fk_column," +
+                     " r.table_schema r_schema, r.table_name r_table, r.column_name r_column" +
+                     " FROM information_schema.key_column_usage k" +
+                     " INNER JOIN information_schema.referential_constraints rUK" +
+                     "         ON k.constraint_name = rUK.constraint_name" +
+                     "        AND k.constraint_schema = rUK.constraint_schema" +
+                     " INNER JOIN information_schema.key_column_usage r" +
+                     "         ON rUK.unique_constraint_name = r.constraint_name" +
+                     "        AND rUK.unique_constraint_schema = r.constraint_schema" +
+                     "        AND r.position_in_unique_constraint IS NULL" +
+                     "        AND r.ordinal_position = k.position_in_unique_constraint" +
+                     " WHERE" +
+                     " k.position_in_unique_constraint IS NOT NULL")) {
             while (rs.next()) {
                 ColumnIdentifier from = new ColumnIdentifier(
-                        rs.getString("FKTABLE_SCHEMA"),
-                        rs.getString("FKTABLE_NAME"),
-                        rs.getString("FKCOLUMN_NAME"));
+                        rs.getString("fk_schema"),
+                        rs.getString("fk_table"),
+                        rs.getString("fk_column"));
                 ColumnIdentifier to = new ColumnIdentifier(
-                        rs.getString("PKTABLE_SCHEMA"),
-                        rs.getString("PKTABLE_NAME"),
-                        rs.getString("PKCOLUMN_NAME"));
+                        rs.getString("r_schema"),
+                        rs.getString("r_table"),
+                        rs.getString("r_column"));
                 repository.registerRelation(from, to);
             }
         }
