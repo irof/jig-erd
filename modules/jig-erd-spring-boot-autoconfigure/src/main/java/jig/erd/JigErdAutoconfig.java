31967939efaf5f203dff9223c93b77282c97038f
irof <irof.ocean@gmail.com>
2025-02-24 23:50:31 +0900

    jig.erd.enabledで無効にできるようにした

--- modules/jig-erd-spring-boot-autoconfigure/src/main/java/jig/erd/JigErdAutoconfig.java
+++ modules/jig-erd-spring-boot-autoconfigure/src/main/java/jig/erd/JigErdAutoconfig.java
@@ -2,11 +2,13 @@ package jig.erd;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 import javax.sql.DataSource;
 
+@ConditionalOnProperty(value = "jig.erd.enabled", matchIfMissing = true)
 @ConditionalOnBean(DataSource.class)
 @AutoConfiguration(after = DataSourceAutoConfiguration.class)
 public class JigErdAutoconfig {
