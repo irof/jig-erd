d948a16f42efa1874c67ee6d190ea7b84e9381cc
irof <irof.ocean@gmail.com>
2025-03-07 17:28:50 +0900

    classでよさげ

--- modules/jig-erd-spring-boot-autoconfigure/src/main/java/jig/erd/JigErdAutoconfig.java
+++ modules/jig-erd-spring-boot-autoconfigure/src/main/java/jig/erd/JigErdAutoconfig.java
@@ -6,10 +6,11 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.sql.DataSource;
 
-@ConditionalOnClass(name = {"javax.sql.DataSource", "org.springframework.web.bind.annotation.RequestMapping"})
+@ConditionalOnClass({DataSource.class, RequestMapping.class})
 @ConditionalOnProperty(value = "jig.erd.enabled", matchIfMissing = true)
 @ConditionalOnBean(DataSource.class)
 @AutoConfiguration(after = DataSourceAutoConfiguration.class)
